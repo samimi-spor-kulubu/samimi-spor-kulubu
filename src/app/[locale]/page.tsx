0ad1631160317dcdf39e90dae471bcea658c3660
alperahmedyakub-oss <275149229+alperahmedyakub-oss@users.noreply.github.com>
2026-08-15 18:05:44 +0200

    Hide: Anasayfa'daki Tesis Turu CTA'sını gizle

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -271,7 +271,7 @@ export default async function Home({
       </section>
 
       {/* FACILITY TOUR CTA */}
-      <section className="bg-brand-surface">
+      <section className="hidden bg-brand-surface">
         <div className="mx-auto max-w-6xl px-4 py-16 sm:px-6 sm:py-20 lg:px-8">
           <Link
             href="/tesis-turu"
